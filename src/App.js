268c1a4a8cd05c4ddd86264b6e1806c3344193c8
Yılmaz Yağız Dokumacı <dokumaciyagiz@gmail.com>
2021-03-19 19:39:38 +0300

    Firs Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
+// İmporting Components
+import Form from "./components/form"
+import TodoList from './components/TodoList';
+
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <h1>To - Do List - 2021</h1>
       </header>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
+      <TodoList/>
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Todo = () => {
+    return(
+        <div className="todo">
+            <li className="todo-item">heyyy</li>
+
+            <button className="complete-btn">
+                <i className="fas fa-check"></i>
+            </button>
+
+            <button className="trash-btn">
+                <i className="fas fa-trash"></i>
+            </button>
+        </div>
+
+    );
+}
+
+export default Todo;
