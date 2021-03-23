132f74cc3f172c9186518aebbf9b6c56f2bcea4e
Yılmaz Yağız Dokumacı <dokumaciyagiz@gmail.com>
2021-03-23 18:17:22 +0300

    add filter

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
-// For Toast Message
-const notify = () => toast("Deleted!");
 const Todo = ({text,todo, todos, setTodos}) => {
 
     // Events
     const deleteHandler = () => {
         setTodos(todos.filter((el) => el.id !== todo.id));
-        <ToastContainer />
  };
 
  const completeHandler = () => {
